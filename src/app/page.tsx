ab32f460fc26a3815f8dd974560d1b3f9c5f95b8
TriptoAfsin <AfsinTripto@gmail.com>
2024-03-03 20:49:12 +0600

    ux: better landing page routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+import SpinnerLoader from "@/components/loaders/SpinnerLoader";
+import { getLocal } from "@/utils/storageUtils";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    if (getLocal("user_info")?.access_token) {
+      router.push("/dashboard");
+    } else {
+      router.push("/auth/login");
+    }
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Gotipath</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Image src={"/images/logo.svg"} alt="logo" width={170} height={170} />
+      <div className="my-5">
+        <SpinnerLoader size="md" variant="primary" />
+      </div>
     </main>
   );
 }
