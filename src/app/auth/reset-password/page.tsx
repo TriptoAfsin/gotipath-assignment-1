a809d698bdc081d1e2579a45f3cf04de95b73874
TriptoAfsin <AfsinTripto@gmail.com>
2024-03-04 10:55:25 +0600

    feat: ui for forgot pass page

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,10 +1,26 @@
+import ForgotPassForm from "@/components/forms/ForgotPassForm";
+import Header from "@/components/layout/Header";
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Register | Gotipath",
+  title: "Reset Password | Gotipath",
 };
 
 function ResetPassword() {
-  return <div>ResetPassword</div>;
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto] h-screen">
+      <Header showAccountMenu={false} />
+      <div className="h-full grid place-content-center w-4/6 max-w-[400px] mx-auto">
+        <div className="w-full">
+          <h1 className="text-3xl font-semibold mb-3">Reset your password</h1>
+          <p className="mb-5">
+            Enter your registered email address to change your Gotipath account
+            password
+          </p>
+          <ForgotPassForm />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ResetPassword;
