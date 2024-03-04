9b9a923d9480865c5aaaca9776a6a9488799a5f1
TriptoAfsin <AfsinTripto@gmail.com>
2024-03-04 16:16:31 +0600

    fix: remove react query dev tools

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 import React from "react";
 
@@ -11,7 +10,6 @@ function Providers({ children }: React.PropsWithChildren) {
   return (
     <QueryClientProvider client={client}>
       <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
