a4be922aad61882b077d073c329c1ba185ada1f6
TriptoAfsin <AfsinTripto@gmail.com>
2024-03-04 19:36:51 +0600

    ux: better not available toast

--- src/app/auth/login/_sections/LoginFooterSection.tsx
+++ src/app/auth/login/_sections/LoginFooterSection.tsx
@@ -12,39 +12,47 @@ function LoginFooterSection() {
     <>
       <p className="text-center font-medium my-4">Login with</p>
       <div className="flex space-x-4 justify-center my-4">
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/github.svg"}
             width={20}
             height={20}
-            onClick={showToast}
             alt="github login"
           />
         </button>
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/google.svg"}
             width={20}
             height={20}
-            onClick={showToast}
             alt="google login"
           />
         </button>
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/jira.svg"}
             width={20}
             height={20}
-            onClick={showToast}
             alt="jira login"
           />
         </button>
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/circle.svg"}
             width={20}
             height={20}
-            onClick={showToast}
             alt="circle login"
           />
         </button>
--- src/app/auth/register/_sections/RegFooterSection.tsx
+++ src/app/auth/register/_sections/RegFooterSection.tsx
@@ -1,11 +1,21 @@
+"use client";
+import { toast } from "@/components/ui/use-toast";
 import Image from "next/image";
 
 function RegFooterSection() {
+  const showToast = () => {
+    toast({
+      title: "This feature is not available yet 😢",
+    });
+  };
   return (
     <>
       <p className="text-center font-medium my-4">Login with</p>
       <div className="flex space-x-4 justify-center my-4">
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/github.svg"}
             width={20}
@@ -13,7 +23,10 @@ function RegFooterSection() {
             alt="github login"
           />
         </button>
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/google.svg"}
             width={20}
@@ -21,7 +34,10 @@ function RegFooterSection() {
             alt="google login"
           />
         </button>
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/jira.svg"}
             width={20}
@@ -29,7 +45,10 @@ function RegFooterSection() {
             alt="google login"
           />
         </button>
-        <button className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition">
+        <button
+          className="border border-border-default p-2.5 rounded shadow-sm hover:bg-bg-fill transition"
+          onClick={showToast}
+        >
           <Image
             src={"/assets/icons/circle.svg"}
             width={20}
